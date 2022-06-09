e56e53585bd4433f04a3be5ab964dfdeeed0a220
RitvikY <72718241+RitvikY@users.noreply.github.com>
2022-06-09 08:56:33 -0400

    Time: 15 ms (93.49%), Space: 63.9 MB (55.97%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -10,22 +10,24 @@ class Solution {
         
         int arr[] = nums;
         
+        //counter will check the # of consecutives from the pointer while total will be the final highest number
         int counter = 0, total = 0;
         
         for(int i= 0; i < arr.length; i++){
             
-            if(i + 1 < arr.length) {
-                
-                
+            
+            if(i + 1 < arr.length) { // this is to prevent Out of bounds error
                 
                 if(arr[i] +1 == arr[i+1]){
                     counter++;
                 } else if (arr[i] == arr[i+1]) {
-                    // do nothing to preserve order
+                    // do nothing to preserve order if next number is the same
                 } else {
                     counter = 0;
                 }
                 
+                
+                // update the highest order
                 if (counter > total){
                     total = counter;
                 }
