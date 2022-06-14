19980dcbd29349c3338cfb082887aa708b4a7613
RitvikY <72718241+RitvikY@users.noreply.github.com>
2022-06-14 17:20:14 -0400

    Time: 12 ms (43.62%), Space: 44.4 MB (37.80%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -1,40 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
         
+        
+        // is a palindrome if an empty string
         if (s.isEmpty() == true){
             return true;
         } 
         
         String news = s.toLowerCase();
-        StringBuffer result = new StringBuffer ("");        
+        StringBuffer result = new StringBuffer ("");  // We use a string buffer since we want to appened letters as we check them      
         
         for(int i = 0; i < news.length(); i++){
             
             char c = news.charAt(i);
             
-            if (Character.isLetter(c) || Character.isDigit(c)){
+            if (Character.isLetter(c) || Character.isDigit(c)){ // checks to make sure the current character is an Alphanumeric character
                 
                 result.append(c);
             }
         }
+          
+        StringBuffer r = new StringBuffer (result); // since string buffers are mutable, we need to create a seperate stringBuffer when performing our comparison
         
         
-       
-        
-        StringBuffer r = new StringBuffer (result);
-        
-        
-
-        Boolean boo = result.toString().equals(r.reverse().toString());
-        
-        System.out.println(boo);
-        
-        System.out.println(result);
-        System.out.println(result.reverse());
-        
-        
-        
-         if (result.toString().equals(r.reverse().toString())){
+        // we need to convert the stringBuffer back into a string before performing our comparison using the toString() method
+        if (result.toString().equals(r.reverse().toString())){ 
              return true;
          }     
         return false;    
