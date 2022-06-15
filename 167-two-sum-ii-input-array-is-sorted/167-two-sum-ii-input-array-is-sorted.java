78081c40bd923319c1bf2758e1fe4edb682a3067
RitvikY <72718241+RitvikY@users.noreply.github.com>
2022-06-15 17:44:00 -0400

    Time: 2 ms (73.40%), Space: 50.2 MB (30.28%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,33 +1,38 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         
-          if (numbers.length == 2){
+        
+        // I took the approach of using 2 pointers for this solution, starting from the leftmost and right most side and checking every combination of each number with the numbers to the right of it in the array
+        
+        
+        
+        // if array is only 2 numbers
+         if (numbers.length == 2){
                 return new int[] {1, 2};
         }
         
-        int counter = 0;
-        int i = 0;
-        System.out.println(numbers.length);
         
-          
-            //[5,25,75] , 100
-            int curr = numbers[i];
-            counter = numbers.length - 1;
-           
-            int checking = numbers[counter];
-            
-            System.out.println(counter);
-
+        
+        int i = 0;        
+        int curr = numbers[i];     
+        int counter = numbers.length - 1;   
+        int checking = numbers[counter];
+        
+        
             while(i <= counter) {
-                checking = numbers[counter];
-                 curr = numbers[i];
                 
-                System.out.println("i: "  + i + "counter: "  + counter );
+                // updating the numbers to the current index
+                checking = numbers[counter];
+                curr = numbers[i];
+                
+                // making sure target and sum are equal
                 if(checking + curr == target){
                  int result[] = new int[] {++i, ++counter};
                  return result;
                 }
                 
+                
+                // choosing whether to update the left hand side or right hand side
                 if (checking + curr < target){
                     i++;
                 } else {
