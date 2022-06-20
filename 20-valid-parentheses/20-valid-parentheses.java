93f7ce18d7de965fcc4376169b2263cd9eb880e2
RitvikY <72718241+RitvikY@users.noreply.github.com>
2022-06-20 13:15:58 -0400

    Time: 3 ms (55.47%), Space: 42.3 MB (31.47%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,12 +1,16 @@
 class Solution {
     public boolean isValid(String s) {
         
+        /* the best way to go about this problem is to implement the solution using a stack. 
+         * By doing this, we are able to pop check if the next character will complete the parenthesis or not */
+        
         Stack<Character> stack = new Stack<Character>();
         
         for(int i= 0; i < s.length(); i++) {
             
             char c = s.charAt(i);
             
+            // we will push onto the stack all the valid starting characters
             if(c == '(' || c == '{' || c == '['){ 
                 
                 stack.push(c);
@@ -16,6 +20,7 @@ class Solution {
                   
                   char peek = stack.peek();
                   
+                  // this will check if the current top of the stack will complete the current character;
                   if ((peek == '(' && c != ')') 
                       ||(peek == '{' && c != '}')
                       ||(peek == '[' && c != ']')) {
