a0f636b6e2e83d8dcd7aa47eeef50f846d136ee8
RitvikY <72718241+RitvikY@users.noreply.github.com>
2022-06-20 10:28:02 -0400

    Time: 1004 ms (8.88%), Space: 42.6 MB (80.71%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        
+        
+        
+        int start = 0, len = s1.length() - 1, end  = len ;
+        
+    
+        char s1arr[] = s1.toCharArray();
+        Arrays.sort(s1arr);
+        
+        String sorteds1 = new String(s1arr);
+        System.out.println(sorteds1);
+        
+        
+        while (start < s2.length() - len) {
+            
+            String sub = s2.substring(start, end+1);
+            //System.out.println(sub);
+
+            
+            char charsSub[] = sub.toCharArray();
+            Arrays.sort(charsSub);
+            String sortedSub = new String(charsSub);
+            
+            if (sortedSub.equals(sorteds1)) {
+                return true;
+            }
+            
+            end++;
+            start++;  
+        }
+        
+    
+        
+        
+        
+        
+        
+        
+        return false;
+        
+        
+        
+        
+        
+        
+    }
+}
