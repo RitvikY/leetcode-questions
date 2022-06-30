c88b52eae5d1838a20a8ce4fe64380d136afd9f2
RitvikY <72718241+RitvikY@users.noreply.github.com>
2022-06-30 09:39:05 -0400

    Time: 0 ms (100.00%), Space: 54.4 MB (39.22%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,6 +1,8 @@
 class Solution {
     public int search(int[] nums, int target) {
         
+        /* simple binary search algo to find position of taget */
+        
         int curr = nums.length-1, left = 0, pivot;
                 
         while (left <= curr){
