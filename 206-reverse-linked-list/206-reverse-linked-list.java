b0a379b00637d47d0b8b46300ecd589ade9ef42e
RitvikY <72718241+RitvikY@users.noreply.github.com>
2022-07-11 08:25:49 -0400

    Time: 0 ms (100.00%), Space: 42.7 MB (70.86%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -10,9 +10,16 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        
+        /* For this solution, we can use 3 pointers in order to maintain the order of
+         * original Linked List */
+        
+        
         ListNode curr = head;
         ListNode first, second = null;
         
+        /* 'first' will store the value of the list we are returning, while the head of 
+         * the list 'second' will take the pointer to the head of the original list */
         while (curr != null) {
             first = second;
             second = curr;
