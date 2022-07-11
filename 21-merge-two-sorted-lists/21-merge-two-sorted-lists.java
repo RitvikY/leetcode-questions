421904303b4ceeec39ee863ac759e3ba0c86b817
RitvikY <72718241+RitvikY@users.noreply.github.com>
2022-07-11 08:40:30 -0400

    Time: 1 ms (78.86%), Space: 42.1 MB (83.26%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -11,11 +11,15 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         
+        /* the best way to go about this solution is to check the head of each
+         * list and iincrement the list which is added onto our new LL */
         
         ListNode curr = new ListNode();
         ListNode head = curr;
     
         while (list1 != null && list2 !=null ) {
+            
+            /* comparing the head of each list */
             if(list1.val < list2.val){
                 head.next = list1;
                 list1 = list1.next;
@@ -28,12 +32,14 @@ class Solution {
 
         }
         
+        /*once 1 list has gone to completion, then we just assign the remaining elements
+         * of the next list to the end. */
         if (list1 == null) {
             head.next = list2;
         } else if (list2 == null) {
             head.next = list1;
         }
-        
+        /* we return curr.next since our head is empty */
         return curr.next;
         
     }
