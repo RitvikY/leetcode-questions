240af95f441d29d6fcd3a60c1308cea8f976e1c1
RitvikY <72718241+RitvikY@users.noreply.github.com>
2022-07-14 08:27:53 -0400

    Time: 0 ms (100.00%), Space: 46.1 MB (34.17%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -12,13 +12,21 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         
+        
+        /* the approach for this solution is to create a 'flag' in order to see if a 
+         * node has been visited. Since we can change the values of the nodes, I set
+         * the flag to be Integer.MAX and return true if we find that as a nodes value.
+         * This is only done becuase we cannot create a flag variable in the ListNode a
+         * and must use the given parameters */
+        
         ListNode curr = head;
         
         if (curr == null) return false;
+        
         while (curr.next != null) {
-            if (curr.val == Integer.MAX_VALUE) return true;
+            if (curr.val == Integer.MAX_VALUE) return true; // checking if a node has been visited
             
-            curr.val = Integer.MAX_VALUE;
+            curr.val = Integer.MAX_VALUE; // setting the flag
             curr = curr.next;
         }
         
