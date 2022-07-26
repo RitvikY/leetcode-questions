c8158084390cd7b01e8962499c32aa89ac9288af
RitvikY <72718241+RitvikY@users.noreply.github.com>
2022-07-26 09:18:03 -0400

    Time: 0 ms (100.00%), Space: 40.6 MB (90.02%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -20,10 +20,14 @@ class Solution {
         return helper(root.left, root.right);
     }
     
+    /* helper function will check to make sure each leaf is symmetric */
     private boolean helper (TreeNode first, TreeNode second) {
         if (first == null && second == null) return true;
         if (first == null || second == null) return false;
         if (first.val != second.val) return false;
+        
+        /* We check each trees left node with the opposite trees right node because in order to be 
+         * symmetric for a tree, it needs to be able to fold onto itself like in the picture*/
         return helper(first.left, second.right) &&  helper (first.right, second.left);
     }
 }
