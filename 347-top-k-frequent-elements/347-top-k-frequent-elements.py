bc2793f18a4e558e49782d7357acb9d886b69263
Ritvik Yaragudipati <72718241+RitvikY@users.noreply.github.com>
2022-09-04 12:53:15 -0400

    Time: 140 ms (70.10%), Space: 18.8 MB (35.28%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -11,11 +11,8 @@ class Solution:
                 
                 
         num = []
-        s = sorted(dic.items(), key=lambda x: x[1], reverse=True)
+        s = sorted(dic.items(), key=lambda x: x[1], reverse=True) # this will order the list by greatest to least value
 
-            
-        
-        print(s)
         for i in range(k):
             k = s[i][0]
             num.append(k)
